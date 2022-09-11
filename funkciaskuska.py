392ca82e44bd5f6d63a3dcd7a0cfa757deb88269
Hubabubajahoda <natalia.kubicova@gmail.com>
2022-09-11 12:02:53 +0200

    My Django Girls app, first commit

---
+++ funkciaskuska.py
@@ -0,0 +1,10 @@
+# funkcia skusobna, subor funkciaskuska.py
+
+def ahoj(meno):
+    print ('Ahoj ' + meno + '!')
+
+dievcata = ['Natalia', 'Lucia', 'Maja']
+
+for meno in dievcata:
+    ahoj (meno)
+    print ("Vitaj na PyCon2022")
---
+++ skusanie.py
@@ -0,0 +1,8 @@
+#overujeme ci vieme napisat#
+
+if 2 > 3:
+    print('Áno, 3 je viac ako 2')
+elif 3>2:
+    print ('Áno, 3 je viac ako 2')
+else:
+    print ('Nie toto nepojde.')
